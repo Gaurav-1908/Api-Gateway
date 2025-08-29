07b742e19fa359477b1e0ad7eb43dba76094c290
Gaurav Gadkari <gaurav.gadkari1908@gmail.com>
2025-08-30 00:05:01 +0530

    node-development

--- server.js
+++ server.js
@@ -6,43 +6,14 @@ const { getResponse } = require("./src/getResponse")
 
 const app = express();
 const PORT = conf.server.port;
+const LISTEN = conf.server.listen;
 
-// middleware
 app.use(express.json());
-
-// round robin counters per service
-
-// helper to get next server for a given service
-
-
-
-
 
 app.use(getService);
 app.use(rateLimiter);
 app.use(getResponse)
 
-// app.use(async (req, res) => {
-//   try {
-//     const target = getNextServer(req.service);
-//     const response = await axios({
-//       method: req.method,
-//       url: target + req.originalUrl.replace(req.service.location, ""),
-//       data: req.body,
-//       headers: req.headers,
-//     });
-
-//     res.status(response.status).send(response.data);
-//   } catch (err) {
-//     if (err.response) {
-//       res.status(err.response.status).send(err.response.data);
-//     } else {
-//       console.log(err);
-//       res.status(500).send("Error forwarding request");
-//     }
-//   }
-// });
-
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, LISTEN, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
